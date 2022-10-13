da26a1015fd1b229f9b86891cc4139fa4bb07724
Alex Lazar <alex2012ro@gmail.com>
2022-10-14 02:26:31 +0300

    chore: add dodoc

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,14 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import 'hardhat-contract-sizer';
+import '@primitivefi/hardhat-dodoc';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
   contractSizer: {
+    runOnCompile: true,
+  },
+  dodoc: {
     runOnCompile: true,
   },
 };
