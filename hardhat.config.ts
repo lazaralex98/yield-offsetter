95a231fbfefbc6f995dfd885411fe89488660ffb
Alex Lazar <alex2012ro@gmail.com>
2022-10-14 02:10:21 +0300

    chore: add contract size

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,12 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-contract-sizer';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
+  contractSizer: {
+    runOnCompile: true,
+  },
 };
 
 export default config;
