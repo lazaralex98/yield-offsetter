832a971135cff3fce66488e3925137fe609b1406
Alex Lazar <alex2012ro@gmail.com>
2022-10-14 02:28:55 +0300

    chore: integrate etherscan verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,9 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import 'hardhat-contract-sizer';
 import '@primitivefi/hardhat-dodoc';
+import '@nomiclabs/hardhat-etherscan';
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
@@ -10,6 +13,9 @@ const config: HardhatUserConfig = {
   },
   dodoc: {
     runOnCompile: true,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
 
