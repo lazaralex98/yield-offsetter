82e058cc14d71b57aa35cba435d72e435e2eab12
Alex Lazar <alex2012ro@gmail.com>
2022-10-15 01:00:08 +0300

    feat: create vault
    factory functionality to create vaults

--- contracts/YieldOffseterFactory.sol
+++ contracts/YieldOffseterFactory.sol
@@ -1,18 +1,62 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import {AavePool} from './interfaces/AaavePool.sol';
+import {WMatic} from './interfaces/WMatic.sol';
 import {YieldOffseterVault} from './YieldOffseterVault.sol';
 
 /// @title YieldOffseterFactory
 contract YieldOffseterFactory {
+    // ============================================
+    // ================ State vars ================
+    // ============================================
+
+    /// interface to the Aave pool
+    AavePool public immutable aavePool;
+
+    /// interface to the WMATIC token
+    WMatic public immutable wMatic;
+
     /// each user is entitled to only 1 YieldOffseterVault
     mapping(address => address) private vaults;
 
+    // ============================================
+    // ================== Events ==================
+    // ============================================
+
+    /// @notice Emitted when a new YieldOffseterVault is created
+    /// @param _owner Owner of the YieldOffseterVault
+    /// @param _vault Address of the YieldOffseterVault
+    event VaultCreated(address indexed _owner, address indexed _vault);
+
+    // ============================================
+    // ================ Constructor ===============
+    // ============================================
+
+    /// @dev TODO this is not supposed to be deployable by anyone but the YieldOffseterFactory
+    constructor(address _aavePool, address _wmatic) {
+        aavePool = AavePool(_aavePool);
+        wMatic = WMatic(_wmatic);
+    }
+
+    // ============================================
+    // =============== Public funcs ===============
+    // ============================================
+
     /// @notice Creates a YieldOffseterVault for the caller
-    function createVault() public returns (address) {}
+    /// @return Address of the newly created YieldOffseterVault
+    function createVault() public returns (address) {
+        require(vaults[msg.sender] == address(0), 'vault already exists');
+        YieldOffseterVault newVault = new YieldOffseterVault(address(aavePool), address(wMatic));
+        vaults[msg.sender] = address(newVault);
+        emit VaultCreated(msg.sender, vaults[msg.sender]);
+        return vaults[msg.sender];
+    }
 
     /// @notice Returns the YieldOffseterVault for `_guy`
     /// @param _guy Address of the vault owner
     /// @return Address of the YieldOffseterVault that `_guy` owns
-    function getVault(address _guy) public view returns (address) {}
+    function getVault(address _guy) public view returns (address) {
+        return vaults[_guy];
+    }
 }
--- docs/YieldOffseterFactory.md
+++ docs/YieldOffseterFactory.md
@@ -10,6 +10,23 @@
 
 ## Methods
 
+### aavePool
+
+```solidity
+function aavePool() external view returns (contract AavePool)
+```
+
+interface to the Aave pool
+
+
+
+
+#### Returns
+
+| Name | Type | Description |
+|---|---|---|
+| _0 | contract AavePool | undefined |
+
 ### createVault
 
 ```solidity
@@ -25,7 +42,7 @@ Creates a YieldOffseterVault for the caller
 
 | Name | Type | Description |
 |---|---|---|
-| _0 | address | undefined |
+| _0 | address | Address of the newly created YieldOffseterVault |
 
 ### getVault
 
@@ -49,6 +66,43 @@ Returns the YieldOffseterVault for `_guy`
 |---|---|---|
 | _0 | address | Address of the YieldOffseterVault that `_guy` owns |
 
+### wMatic
+
+```solidity
+function wMatic() external view returns (contract WMatic)
+```
+
+interface to the WMATIC token
+
+
+
+
+#### Returns
+
+| Name | Type | Description |
+|---|---|---|
+| _0 | contract WMatic | undefined |
+
+
+
+## Events
+
+### VaultCreated
+
+```solidity
+event VaultCreated(address indexed _owner, address indexed _vault)
+```
+
+Emitted when a new YieldOffseterVault is created
+
+
+
+#### Parameters
+
+| Name | Type | Description |
+|---|---|---|
+| _owner `indexed` | address | Owner of the YieldOffseterVault |
+| _vault `indexed` | address | Address of the YieldOffseterVault |
 
 
 
